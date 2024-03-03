44c99820b3c77550c07e3aebe78af8f83fd4e867
Zubair Irshad <Muhammad Zubair Irshad>
2024-03-03 03:44:14 -0800

    add nerfmae segmentation dataset

--- create_new_split.py
+++ create_new_split.py
@@ -1,12 +1,18 @@
 import numpy as np
+
+# Load the split file
 dataset_split = '/wild6d_data/zubair/MAE_complete_data/front3d_split.npz'
 with np.load(dataset_split) as split:
     train_scenes = split["train_scenes"]
+    test_scenes = split["test_scenes"]
+    val_scenes = split["val_scenes"]
 
-scenes_remove = '00326-u9rPN5cHWBg_9.npz'
-scenes_remove = scenes_remove.split('.')[0]
+# Remove the specific scene from train_scenes
+scene_to_remove = "00326-u9rPN5cHWBg_9"
+train_scenes = [scene for scene in train_scenes if scene != scene_to_remove]
 
-train_scenes = [scene for scene in train_scenes if scene != scenes_remove]
+# Save the modified train_scenes into a new file
+new_train_scenes_file = '/wild6d_data/zubair/MAE_complete_data/new_train_scenes.npz'
+np.savez(new_train_scenes_file, train_scenes=train_scenes, test_scenes=test_scenes, val_scenes=val_scenes)
 
-#save the new split
-np.savez('/wild6d_data/zubair/MAE_complete_data/front3d_split.npz', train_scenes=train_scenes, test_scenes=split["test_scenes"], val_scenes=split["val_scenes"])
+print("New train scenes file saved successfully.")
